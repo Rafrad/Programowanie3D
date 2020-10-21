31a290b9e1d3da63c8c2f2f469261d8091d22143
Rafal <rafal@pop-os.localdomain>
2020-10-21 09:47:37 +0200

    Initial Commit

---
+++ build/src/Application/CMakeFiles/application.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rafal/Documents/P3D/src/Application/application.cpp" "/home/rafal/Documents/P3D/build/src/Application/CMakeFiles/application.dir/application.cpp.o"
+  "/home/rafal/Documents/P3D/src/Application/utils.cpp" "/home/rafal/Documents/P3D/build/src/Application/CMakeFiles/application.dir/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/3rdParty/include"
+  "../src"
+  "../src/3rdParty/src/glfw3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/rafal/Documents/P3D/build/src/3rdParty/src/glfw3/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/Exercises/Colors/CMakeFiles/Colors.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Colors.dir/app.cpp.o"
+  "CMakeFiles/Colors.dir/main.cpp.o"
+  "Colors"
+  "Colors.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Colors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
